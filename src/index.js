372327d982eb5018e623c88df432eed43e16ab73
yokinist <ikd618@gmail.com>
2020-03-11 04:38:19 +0900

    :sparkles: TypeScript

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { render } from 'preact';
-
-const App = () => <h1>OK</h1>;
-
-render(<App />, document.getElementById('root'));
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { h, render, FunctionalComponent } from 'preact';
+
+type Props = {
+  message?: string;
+}
+
+const App: FunctionalComponent<Props> = ({ message = 'JP' }) => <p>Hello, {message}</p>;
+
+render(<App message={'Tokyo'} />, document.getElementById('root'));
