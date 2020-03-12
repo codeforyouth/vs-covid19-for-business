22b7a3b82ecfc8295b470720a011fdfb57ea7e07
yokinist <ikd618@gmail.com>
2020-03-12 10:44:47 +0900

    :lipstick: Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import { h, render, FunctionalComponent } from 'preact';
-import { useEffect } from 'preact/hooks';
 import { useFetch, useFormat } from './hooks';
 import { SHEET_KEY, getCSVUrl, PROXY_HOST } from './constants';
-
+import { Card, SearchBox } from './components';
 const App: FunctionalComponent = () => {
   const { response, error, isLoading } = useFetch(
     PROXY_HOST + '/?url=' + encodeURIComponent(getCSVUrl(SHEET_KEY)),
   );
   const { data } = useFormat(response?.data);
-
   return (
     <div>
+      <SearchBox />
       {isLoading && <p>読込中</p>}
-      {!isLoading &&
-        data &&
-        data.map(item => (
-          <div>
-            <h3>サービス名称: {item.サービス名称}</h3>
-            <p>分野: {item.分野}</p>
-            <p>詳細: {item.詳細}</p>
-          </div>
-        ))}
+      {!isLoading && data && data.map((item, i) => <Card key={i} {...item} />)}
       {!isLoading && error && <p>{error.name + ':' + error.message}</p>}
     </div>
   );
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,71 @@
+import { h, FunctionalComponent } from 'preact';
+import styled from 'styled-components';
+import images from '../assets/images/*.png';
+
+const SearchBox: FunctionalComponent = () => (
+  <Container>
+    <h1 id="title">
+      <img src={images.title} alt="VS COVID-19" />
+      <div class="subtitle">
+        <a href="https://twitter.com/search?q=%23%E6%B0%91%E9%96%93%E6%94%AF%E6%8F%B4%E6%83%85%E5%A0%B1%E3%83%8A%E3%83%93">
+          #民間支援情報ナビ
+        </a>
+      </div>
+    </h1>
+    <div class="searchboxid">
+      <span class="sitedesc">
+        企業等による新型コロナウイルス感染症対策支援サービスをまとめました（出典:
+        <a href="https://docs.google.com/spreadsheets/d/1IiHUk3D_b6e5BfqFG3ZBxQ8X-QVACdY7CeQeG6C7S1w/">
+          政府オープンデータ
+        </a>
+      </span>
+      <input
+        type="text"
+        id="searchbox"
+        placeholder="検索する単語をご入力ください"
+      />
+    </div>
+  </Container>
+);
+
+const Container = styled.div`
+  > h1 {
+    margin: 0;
+    padding: 0.8em 0.3em 0.1em 0.3em;
+    color: white;
+    text-align: center;
+    font-size: 7vw;
+    > img {
+      width: 50vw;
+    }
+    > .subtitle {
+      color: #333;
+      font-size: 3vw;
+      > a {
+        color: #333 !important;
+        text-decoration: none;
+      }
+    }
+  }
+  > .searchboxid {
+    text-align: center;
+    > .sitedesc {
+      font-size: 80%;
+      display: inline-block;
+      width: 80vw;
+      > a {
+        color: black !important;
+      }
+    }
+    > input {
+      width: 60vw;
+      margin: 0.5em 0.2em 0.2em 0.5em;
+      font-size: 24px;
+      border: 2px solid black;
+      line-height: 1.3em;
+      padding: 0.1em 0.3em;
+    }
+  }
+`;
+
+export default SearchBox;
