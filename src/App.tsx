74129505b85933133a7fd418b797e6ea75ad003c
yokinist <ikd618@gmail.com>
2020-03-14 20:00:59 +0900

    :wrench: FunctionComponent -> FunctionalComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { h, render, FunctionalComponent, FunctionComponent } from 'preact';
+import { h, render, FunctionalComponent } from 'preact';
 import { useEffect } from 'preact/hooks';
 import { SHEET_URL } from './constants';
 import { AppContainer } from './containers';
 import { SearchBox, Content } from './components';
 import GlobalStyle from './styles';
 
-const AppComponent: FunctionComponent = () => {
+const AppComponent: FunctionalComponent = () => {
   const { fetchSupports } = AppContainer.useContainer();
-
   useEffect(() => {
     fetchSupports(SHEET_URL);
   }, []);
