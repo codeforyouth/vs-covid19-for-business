42ce7807b7960fdd221263d04deceffdf544d1ae
yokinist <ikd618@gmail.com>
2020-03-15 01:11:12 +0900

    :art: Add colors

---
+++ src/shared/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  yellow: '#f6c744',
+  purple: '#4A6EED',
+  orange: '#F07A0C',
+  green: '#3BA599',
+  blue: '#5AA9E2',
+  violet: '#A626D3',
+  textGray: '#a4a4a4',
+  textBlack: '#333',
+  bgLightGray: '#f3f3f4',
+  borderGray: '#f3f3f4',
+  white: '#ffffff',
+};
