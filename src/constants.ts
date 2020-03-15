6ebd2245bab59643b103e972eb0d0bbcfac8fbb7
yokinist <ikd618@gmail.com>
2020-03-15 14:52:01 +0900

    :+1: Add meta & url info

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,14 @@ export const SHEET_URL =
   'https://app.sabae.cc/api/googlespreadsheet.json?key=2PACX-1vSFMNp5HcRNOF5MrAujEUWR1dIoX2mncMEWTbPlVAaJqKWiq831-6gnCyI7n_G8YfPqNQXrfwyVjyHL';
 
 export const LAYOUT_WIDTH = 920;
+
+type keys = 'development' | 'production';
+export const BASE_URL = {
+  development: 'http://localhost:1234/',
+  production: 'https://vs-covid19.now.sh/',
+}[(process.env.NODE_ENV as keys) || 'development'];
+
+export const Meta = {
+  title: 'VS COVID-19 #民間支援情報ナビ',
+  description: '企業等による新型コロナウイルス感染症対策支援サービスまとめ',
+};
