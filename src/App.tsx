e5a49141e5e71cc0d9be08fd13d0d91dc00f05d7
yokinist <ikd618@gmail.com>
2020-04-05 01:26:25 +0900

    :sparkles: Filter supports by params

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import { h, render, FunctionalComponent } from 'preact';
+import { h, render, FunctionalComponent, ComponentChild } from 'preact';
 import { useEffect } from 'preact/hooks';
+import Router, { Route } from 'preact-router';
 import { SHEET_URL } from './constants';
 import { AppContainer } from './containers';
 import { SearchBox, Content } from './components';
 import GlobalStyle from './styles';
 
-const AppComponent: FunctionalComponent = () => {
-  const { fetchSupports } = AppContainer.useContainer();
+type MatchKeys = 'q' | 'target' | 'category';
+type RouteProps = {
+  matches: { [key in MatchKeys]: string };
+  url: string;
+  path: string;
+};
+
+const AppComponent: FunctionalComponent<RouteProps &
+  ComponentChild> = props => {
+  const {
+    fetchSupports,
+    handleSetWord,
+    handleSetTarget,
+    handleSetCategory,
+    supportsData,
+  } = AppContainer.useContainer();
+
   useEffect(() => {
     fetchSupports(SHEET_URL);
   }, []);
+
+  useEffect(() => {
+    const { q, target, category } = props.matches;
+    if (supportsData.response) {
+      if (q) handleSetWord(q);
+      if (target) handleSetTarget(target);
+      if (category) handleSetCategory(category);
+    }
+  }, [props.matches]);
+
   return (
     <div>
       <GlobalStyle />
-      <SearchBox />
+      <SearchBox {...props} />
       <Content />
     </div>
   );
@@ -23,7 +49,9 @@ const App: FunctionalComponent = () => {
   const { Provider } = AppContainer;
   return (
     <Provider>
-      <AppComponent />
+      <Router>
+        <Route path="/" component={AppComponent} />
+      </Router>
     </Provider>
   );
 };
