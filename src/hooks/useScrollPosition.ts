1f8d26e5b55f5a49c32fb1b308fc441d4048c959
yokinist <ikd618@gmail.com>
2020-04-13 05:50:14 +0900

    :wrench: useScrollPosition

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -1,11 +1,11 @@
-import { useEffect, useState, useCallback } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 
 export const useScrollPosition = (): number => {
   const [scrollPosition, setScrollPosition] = useState(0);
   useEffect(() => {
     let tmpPosition = 0;
     let isPending = false;
-    const handleScroll = useCallback(() => {
+    const handleScroll = (): void => {
       tmpPosition = window.scrollY;
       if (!isPending) {
         window.requestAnimationFrame(() => {
@@ -14,7 +14,7 @@ export const useScrollPosition = (): number => {
         });
         isPending = true;
       }
-    }, []);
+    };
     window.addEventListener('scroll', handleScroll);
     return (): void => {
       window.removeEventListener('scroll', handleScroll);
